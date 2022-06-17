9ba558cb31c242b397885c86c5019a1a2ce82dbe
kapoor-ishaan <f20212091@goa.bits-pilani.ac.in>
2022-06-17 17:53:27 +0530

    Fixed the search bug and added supoort for drive, sheets and slides

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ let current_search = 'google'
 document.querySelector('.search_input').addEventListener('keyup', e => {
     const input = e.target.value;
     let search_engine = input.split(' ')[0].toLowerCase();
-    let search_query = input.split(' ').splice(0).join(' ');
+    let search_query = input.split(' ').slice(1).join(' ');
     switch(search_engine){
         case 'google':
         case 'youtube':
+        case 'drive':
+        case 'slides':
+        case 'sheets':
         case 'docs': break;
         default: search_engine = 'google';
         search_query = input;
@@ -26,17 +29,41 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
                 image.src = 'docs.png';
                 image.alt = 'Google Docs icon';
                 break;
+            case 'drive':
+                image.src = 'drive.png';
+                image.alt = 'Google Drive icon';
+                break;
+            case 'slides':
+                image.src = 'slides.png';
+                image.alt = 'Google Slides icon';
+                break;
+            case 'sheets':
+                image.src = 'sheets.png';
+                image.alt = 'Google Sheets icon';
+                break;
         }
     }
-    if(e.key === 'Enter')
+    if(e.key === 'Enter') {
+        let search_engine_url
         switch(search_engine) {
             case 'docs':
-                window.location = `https://docs.google.com/document/?q=${search_query}`;
+                search_engine_url = `https://docs.google.com/document/?q=`
                 break;
             case 'youtube': 
-                window.location = `https://www.youtube.com/results?search_query=${search_query}`;
+                search_engine_url = `https://www.youtube.com/results?search_query=`;
+                break;
+            case 'drive': 
+                search_engine_url = `https://drive.google.com/drive/search?q=`;
+                break;
+            case 'slides':
+                search_engine_url = `https://docs.google.com/presentation/?q=`;
+                break;
+            case 'sheets':
+                search_engine_url = `https://docs.google.com/spreadsheets/?q=`;
                 break;
             default: 
-                window.location = `https://www.google.com/search?q=${search_query}`;
+                search_engine_url = `https://www.google.com/search?q=`;
         }
+        window.location = search_engine_url + search_query;
+    }
 });
